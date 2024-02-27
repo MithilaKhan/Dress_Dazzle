f3d36c3a6fab2763a330402c9afca03e903e3bca
Mithila Khan <mithilakhan082@gmail.com>
2024-02-27 12:26:34 +0600

    removed map

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Map from '../Components/Map/Map';
+// import Map from '../Components/Map/Map'; 
 import HereToHelp from './HereToHelp';
 import GetInTouch from './GetInTouch';
 
 const page = () => {
     return (
         <div className='z-10'>       
-            <Map/>
+            {/* <Map/>  */}
             <HereToHelp/>
             <GetInTouch/>
         </div>
